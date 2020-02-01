1865a0a30e2f76db482991cbb0c606711a9a9da5
Eduardo R <EDD-3@users.noreply.github.com>
2020-01-31 18:52:28 -0700

    injection_container created

--- lib/features/news/presentation/bloc/news_event.dart
+++ lib/features/news/presentation/bloc/news_event.dart
@@ -3,3 +3,10 @@ import 'package:equatable/equatable.dart';
 abstract class NewsEvent extends Equatable {
   const NewsEvent();
 }
+
+class GetNewsEvent extends NewsEvent{
+  @override
+  // TODO: implement props
+  List<Object> get props => [];
+
+}
